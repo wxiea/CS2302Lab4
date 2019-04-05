a85e07c63f2a96fc664a682f060bc2e5bfe53593
wxiea <43360560+wxiea@users.noreply.github.com>
2019-04-05 16:59:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -106,18 +106,120 @@ def SearchAndPrint(T,k):
         print(k,'found',end=' ')
         print('node contents:',node.item)
 
-def Min(T):
-    
+def Sortlist(T,L):
+    if T.isLeaf:
+        for i in range(len(T.item)):
+            L.append(T.item[i])#leaf
+    else:
+        for j in range(len(T.item)):
+            Sortlist(T.child[j], L)
+            L.append(T.item[j])#the node
+        Sortlist(T.child[len(T.item)], L)#all the leaf
+    return L
 
- 
+def MinAtDepth(T, d): 
+    if T.isLeaf and d != 0:
+        return False
+    elif d == 0:
+        return T.item[0]
+    else:
+        return MinAtDepth(T.child[0], d-1)
 
-L = [30, 50, 10, 20, 60, 70, 100, 40, 90, 80, 110, 120, 1, 11 , 3, 4, 5,105, 115, 200, 2, 45, 6]
-T = BTree()    
+def MaxAtDepth(T,d): 
+    if T.isLeaf and d != 0:
+        return False
+    elif d == 0:
+        return T.item[-1]
+    else:
+        return MaxAtDepth(T.child[-1], d-1)
+
+
+def NumOfNodes(T,d):
+    if T.isLeaf and d != 0:
+        return False
+    elif d == 0:
+        return 0
+    elif d != 0:
+        for i in range(len(T.item)+1):
+            return PrintHeight(T.child[i],d-1)+1
+
+
+
+def PrintHeight(T,d):
+    if T.isLeaf and d != 0:
+        return False
+    elif d == 0:
+        print(T.item)
+    elif d == 1:
+        for i in range(len(T.item)+1):
+            print(T.child[i].item)
+    elif d != 0 or d != 1:
+        for i in range(len(T.item)+1):
+            PrintHeight(T.child[i],d-1)
+
+"""              
+def NumofNode(T):   
+    if IsFull(T):
+        return 0
+    else:
+        for i in range(len(T.item)):
+            return NumofNode(T.child[i]) + 1
+
+"""
+
+def NumofNode(T): 
+    if T.isLeaf:
+        return 0
+    else:
+        count = 0
+        for i in range(len(T.item)):
+            count += 1+NumofNode(T.child[i])
+        return count
+
+
+def NumofLeaves1(T):
+    if T.isLeaf:
+        return 1
+    else:
+        for i in range(len(T.item)):
+            return NumofLeaves(T.child[len(T.item)])
+
+
+def KeyAtDepth(T, k):
+    # Returns node where k is, or None if k is not in the tree
+    if k in T.item:
+        return T
+    if T.isLeaf:
+        return None
+    return 1 + KeyAtDepth(T.child[len(T.item)], k)
+
+
+
+#L = [30, 50, 10, 20, 60, 70, 100, 40, 90, 80, 110, 120, 1, 11, 3, 4, 5, 105, 115, 200, 2, 45, 6]
+L = [20,50,78,23,54,95,7,55,23,44,15,74,110,120,152,236,204,153,54,78,91]
+#L = [20,50,78,23,54]
+T = BTree()
 for i in L:
-    Insert(T,i)
-    
-PrintD(T,'') 
-Print(T)
-print(height(T))
+    #print('Inserting', i)
+    Insert(T, i)
+    #PrintD(T, '')
+    #Print(T)
+    #print('\n####################################')
+Print(T.child[0])
 
-print(Min(T))
+print(T.child[0].child[0].item)
+
+for i in range(len(T.item)+1):
+    print(T.child[i].item)
+    
+print('height', height(T))
+list = []
+print(Sortlist(T, list))
+print('minimum', MinAtDepth(T,1))
+print('maximum ', MaxAtDepth(T,1))
+#print(NumOfNodes(T,2))
+PrintHeight(T,2)
+print(NumofNode(T))
+print(NumofLeaves(T))
+print(KeyAtDepth(T,3))
+
